75de0f7ca693747edcdd69140a07dfac760d0ff7
emefielezn <emefielengozi54@gmail.com>
2023-01-24 15:59:12 -0800

    scripts for task 2 to 6

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module creates a class named Square"""
+
+
+class Square:
+    """A class named Square
+    Attributes:
+    attr1 (size): size of square
+    """
+
+    def __init__(self, size):
+
+    """
+    Args:
+    size: size for __size attribute of class instance
+    """
+
+    self type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
