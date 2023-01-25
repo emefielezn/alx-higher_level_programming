6f371dd7ff272bd635b523db5666db094d0bce76
emefielezn <emefielengozi54@gmail.com>
2023-01-24 16:19:28 -0800

    changes to task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,12 +8,9 @@ class Square:
     Attributes:
     attr1 (size): size of square
     """
-
     def __init__(self, size):
-
-    """
-    Args:
-    size: size for __size attribute of class instance
-    """
-
-    self.__size = size
+        """
+        Args:
+        size: size for __size attribute of class instance
+        """
+        self.__size = size
