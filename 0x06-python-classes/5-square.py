9ffee8c1cc61222b5d26f19ab0f1829086331f17
emefielezn <emefielengozi54@gmail.com>
2023-01-24 19:11:12 -0800

    changes to task5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -56,11 +56,5 @@ class Square:
         if (self.__size == 0):
             print()
             return
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            for x in range(self.__position[0]):
-                print(" ", end="")
-            for y in range(self.__size):
-                print("#", end="")
-            print()
+        else:
+            print("#", end="")
