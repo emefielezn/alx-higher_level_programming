75648c128b8f96de059f911d669ec243f0d5182b
emefielezn <emefielengozi54@gmail.com>
2023-01-24 16:23:33 -0800

    changes to task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,16 +8,13 @@ class Square:
     Attributes:
     attr1 (size): size of square
     """
-
     def __init__(self, size):
-
-    """
-    Args:
-    size: size for __size attribute of class instance
-    """
-
-    self type(size) != int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+        """
+        Args:
+        size: size for __size attribute of class instance
+        """
+        self type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
