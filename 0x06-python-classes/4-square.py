d8c1ac43d64c547d6993266befccfed1f5c1afb1
emefielezn <emefielengozi54@gmail.com>
2023-01-24 18:44:48 -0800

    changes to task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,40 +8,46 @@ class Square:
     Attributes:
     attr1 (size): size of square
     """
-
-    def __init__(self, size):
-
-    """
-    Args:
-    size: size for __size attribute of class instance
-    """
-
-    self type(size) != int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    def __init__(self, size=0):
+        """
+        Args:
+        size: size for __size attribute of class instance
+        """
+        self.__size =size
+        try:
+            if not (isinstance(self.__size, int)):
+                raise TypeError
+        except TypeError:
+            print("size must be an integer")
+        try:
+            if self.__size < 0:
+                raise ValueError
+        except ValueError:
+            print("size must be >= 0")
 
     def area(self):
-    """Calculates the area based on size of square
-    Returns:
-    int: The return value. Returns the area
-    """
-
-    return self.__size * self.__size
+        """Calculates the area based on size of square
+        Returns:
+        int: The return value. Returns the area
+        """
+        return self.__size * self.__size
 
     @property
     def size(self):
         """Gets the size of the class instance"""
-
         return self.__size
 
     @size.setter
     def size(self, value):
         """Sets the size of the class instance"""
-
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+        try:
+            if not (isinstance(self.__size, int)):
+                raise TypeError
+        except TypeError:
+            print("size must be an integer")
+        try:
+            if self.__size < 0:
+                raise ValueError
+        except ValueError:
+            print("size must be >= 0") 
         self.__size = value
