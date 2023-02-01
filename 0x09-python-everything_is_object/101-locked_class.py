b45bb3cf78c346d58021229beb2be2ddcea09f57
emefielezn <emefielengozi54@gmail.com>
2023-02-01 14:54:30 -0800

    task 100 to 104

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This class lockedclass
+"""
+
+
+class LockedClass:
+    """no attribute"""
+
+    @staticmethod:
+        def play():
+            print ("no class or attribute")
