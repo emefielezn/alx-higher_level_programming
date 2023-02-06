bc4575d3337f9af5efc081d8d2b64532a8a7b7fb
emefielezn <emefielengozi54@gmail.com>
2023-02-06 14:24:21 -0800

    task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class named MyList"""
+
+
+class MyList(list):
+    """A class named MyList
+    Attributes:
+    attr1(print_sorted): prints sorted list
+    """
+
+    def print_sorted(self):
+        """prints instance"""
+
+        print(sorted(self))
