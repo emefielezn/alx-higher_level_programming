a8c03bec30d49e811994e9cd0906a0aca988f285
emefielezn <emefielengozi54@gmail.com>
2023-02-07 08:48:11 -0800

    task 4 to 6

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """returns true or false based on inheritance"""
 
-    if isinstance(obj, a_class) is True type(obj) != a_class:
+    if isinstance(obj, a_class) is True and issubclass(obj, a_class) is True:
         return True
     else:
         False
