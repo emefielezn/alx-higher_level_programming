9f5e5a756ab4ecc48a3e431ecf0f2440f355b52e
emefielezn <emefielengozi54@gmail.com>
2023-02-07 08:17:54 -0800

    task 3 and 4

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true or false based on inheritance"""
+
+    return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """returns true or false based on inheritance"""
+
+    if isinstance(obj, a_class) is True type(obj) != a_class:
+        return True
+    else:
+        False
