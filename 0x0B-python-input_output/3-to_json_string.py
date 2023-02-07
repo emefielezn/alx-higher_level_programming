6416e78b845c06d35bfd4a15c1bce5b2f78c0e32
emefielezn <emefielengozi54@gmail.com>
2023-02-07 15:08:59 -0800

    task 4 and 5

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines the to_json_string function"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)
+    Args:
+    my_obj (str): object to return JSON representation
+    """
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines the to_json_string function"""
+
+
+import json
+
+def from_json_string(my_str):
+    """Returns the JSON representation of an object (string)
+    Args:
+    my_obj (str): object to return JSON representation
+    """
+    return json.loads(my_str)
