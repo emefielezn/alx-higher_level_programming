81575ddad96c248ab750157e4f7f78d0121ea44c
emefielezn <emefielengozi54@gmail.com>
2023-05-23 21:08:25 +0000

    tasks 0 to 8

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+t request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+let resultDict = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    resultDict = JSON.parse(body);
+    console.log(resultDict.title);
+  }
+});
---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+t request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+let text;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    text = body;
+    fs.writeFile(process.argv[3], text, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
